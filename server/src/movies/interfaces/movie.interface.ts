a3bdea8617c72f326fa19882cee25c537f04e0b9
Andree <andree09requejo@gmail.com>
2026-03-18 17:50:08 -0500

    feat: agregar interfaces Movie, MovieDetail y Genre

---
+++ server/src/movies/interfaces/movie.interface.ts
@@ -0,0 +1,50 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  release_date: string;
+  vote_average: number;
+  vote_count: number;
+  genre_ids: number[];
+  popularity: number;
+}
+
+export interface MovieDetail extends Movie {
+  runtime: number;
+  genres: { id: number; name: string }[];
+  tagline: string;
+  budget: number;
+  revenue: number;
+  status: string;
+  credits?: {
+    cast: {
+      id: number;
+      name: string;
+      character: string;
+      profile_path: string | null;
+    }[];
+    crew: {
+      id: number;
+      name: string;
+      job: string;
+      profile_path: string | null;
+    }[];
+  };
+  videos?: {
+    results: {
+      id: string;
+      key: string;
+      name: string;
+      site: string;
+      type: string;
+    }[];
+  };
+  similar?: { results: Movie[] };
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
