97f85ba053f1456c41fc63a9b74b1f3529a489de
Andree <andree09requejo@gmail.com>
2026-03-20 15:27:54 -0500

    feat: configurar TypeORM y módulo de configuración global

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,31 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, MoviesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: true,
+      extra: {
+        sslmode: 'require',
+      },
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    MoviesModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
