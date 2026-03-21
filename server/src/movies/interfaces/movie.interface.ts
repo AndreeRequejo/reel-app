18c19fe39e69c7071be0c43f4c69d113a8be1a16
Andree <andree09requejo@gmail.com>
2026-03-21 12:19:41 -0500

    feat: agregar interfaces TmdbDetailResponse y TmdbResponse para manejar detalles y respuestas de la API de TMDb

--- server/src/movies/interfaces/movie.interface.ts
+++ server/src/movies/interfaces/movie.interface.ts
@@ -6,29 +6,22 @@ export interface Movie {
   backdrop_path: string | null;
   release_date: string;
   vote_average: number;
-  vote_count: number;
-  genre_ids: number[];
-  popularity: number;
 }
 
 export interface MovieDetail extends Movie {
-  runtime: number;
-  genres: { id: number; name: string }[];
-  tagline: string;
-  budget: number;
-  revenue: number;
+  genres: Genre[];
   status: string;
   credits?: {
     cast: {
       id: number;
       name: string;
-      character: string;
+      character?: string;
       profile_path: string | null;
     }[];
     crew: {
       id: number;
       name: string;
-      job: string;
+      job?: string;
       profile_path: string | null;
     }[];
   };
@@ -41,7 +34,6 @@ export interface MovieDetail extends Movie {
       type: string;
     }[];
   };
-  similar?: { results: Movie[] };
 }
 
 export interface Genre {
