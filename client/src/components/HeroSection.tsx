3412c22f9d0bb5287274b98292cc7a4a5a552680
Andree <andree09requejo@gmail.com>
2026-03-23 16:26:27 -0500

    feat: simplificar la importación de getTrendingMovies desde actions

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { getTrendingMovies } from "@/actions/movies/get-trending";
+import { getTrendingMovies } from "@/actions";
 import { useEffect, useState } from "react";
 import Image from 'next/image';
 import { Movie } from "@/interfaces/movie";
