833b7e4be432c1045d428cf17e982843bc182982
Andree <andree09requejo@gmail.com>
2026-03-23 15:31:19 -0500

    feat: agregar interfaces para la gestión de películas y sus detalles

---
+++ client/src/interfaces/movie.ts
@@ -0,0 +1,49 @@
+export interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  release_date: string;
+  vote_average: number;
+}
+
+export interface MovieDetail extends Movie {
+  genres: Genre[];
+  status: string;
+  credits?: {
+    cast: {
+      id: number;
+      name: string;
+      character?: string;
+      profile_path: string | null;
+    }[];
+    crew: {
+      id: number;
+      name: string;
+      job?: string;
+      profile_path: string | null;
+    }[];
+  };
+  videos?: {
+    results: {
+      id: string;
+      key: string;
+      name: string;
+      site: string;
+      type: string;
+    }[];
+  };
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface MovieResponse<TMovie = Movie> {
+  page: number;
+  total_pages: number;
+  total_results: number;
+  results: TMovie[];
+}
