dd2415752b137fa86f6ac805d3e8781c69197b4a
Andree <andree09requejo@gmail.com>
2026-03-23 15:35:34 -0500

    feat: eliminar el componente Navbar del layout principal

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { Footer, Navbar } from "@/components";
+import { Footer } from "@/components";
 
 export default function HomeLayout({
   children,
@@ -7,8 +6,7 @@ export default function HomeLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen bg-background p-6">
-      <Navbar onMovieSelect={() => {}} />
+    <main>
       <div>{children}</div>
       <Footer />
     </main>
